bd624f0f6e26faf42c71e1e3f531fec743349dfe
Yncy0 <yncyncy0@gmail.com>
2025-07-31 20:44:00 +0800

    update: renderer canvas size adjust based on the width and height of the <canvas /> element

--- src/components/CoffeeWithCat.vue
+++ src/components/CoffeeWithCat.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/Addons.js'
 
@@ -13,28 +13,37 @@ let ambientLight: THREE.AmbientLight
 let directionalLight: THREE.DirectionalLight
 
 let mixer: THREE.AnimationMixer
-const clock: THREE.Clock = new THREE.Clock
+const clock: THREE.Clock = new THREE.Clock()
+
+const container = ref(null)
 
 onMounted(() => {
   init()
+
+  window.addEventListener('resize', onWindowResize)
 })
 
 function init() {
   scene = new THREE.Scene()
 
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
-  camera.position.set(0, 5, 10)
+  camera = new THREE.PerspectiveCamera(
+    75,
+    container.value.clientWidth / container.value.clientHeight,
+    0.1,
+    100,
+  )
+  camera.position.set(-2, 3, 13)
 
   ambientLight = new THREE.AmbientLight(0xffffff, 1)
   directionalLight = new THREE.DirectionalLight(0xffffff, 2)
 
-  directionalLight.position.set(0, 5 , 5)
+  directionalLight.position.set(0, 5, 5)
 
   scene.add(camera, ambientLight, directionalLight)
 
   const canvas = document.querySelector('#bg') as HTMLCanvasElement
   renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true })
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setSize(container.value.clientWidth, container.value.clientHeight)
   renderer.setPixelRatio(window.devicePixelRatio)
 
   loadGLTF()
@@ -47,8 +56,7 @@ function loadGLTF() {
 
   gltfLoader.load(url, (gltf) => {
     const mesh = gltf.scene
-    mesh.position.set(-5, 2, -3)
-    mesh.rotation.set(degreeToRadians(30), degreeToRadians(0), degreeToRadians(0))
+    mesh.rotation.set(degreeToRadians(35), degreeToRadians(-10), degreeToRadians(10))
     scene.add(mesh)
 
     const animations = gltf.animations
@@ -60,7 +68,6 @@ function loadGLTF() {
   })
 }
 
-
 function animate() {
   const mixerUpdateDelta = clock.getDelta()
 
@@ -69,10 +76,22 @@ function animate() {
   renderer.render(scene, camera)
 }
 
+function onWindowResize() {
+  const width = container.value.clientWidth
+  const height = container.value.clientHeight
+  renderer.setSize(width, height)
+  camera.aspect = width / height
+  camera.updateProjectionMatrix()
+}
 </script>
 
 <template>
-  <canvas id="bg" />
+  <canvas id="bg" ref="container" />
 </template>
 
-
+<style scoped>
+#bg {
+  width: 100%;
+  height: 100dvh;
+}
+</style>
