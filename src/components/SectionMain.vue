31cf53dfa957a62098138a05bb06623e14863dfd
Yncy0 <yncyncy0@gmail.com>
2025-07-31 22:08:41 +0800

    finished: changing layout

--- src/components/SectionMain.vue
+++ src/components/SectionMain.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
 import AboutMe from './AboutMe.vue'
-import AppFooter from "./AppFooter.vue"
-import AppHeader from './AppHeader.vue';
+import AppFooter from './AppFooter.vue'
+import AppHeader from './AppHeader.vue'
 
-import { Button } from './ui/button';
+import { Button } from './ui/button'
 </script>
 
 <template>
-  <section class="flex flex-row min-h-dvh pt-20">
+  <section class="flex flex-row min-h-dvh pt-20 gap-10">
     <section class="flex flex-col w-[50%] gap-5">
       <AboutMe />
-      <AppHeader />
-
       <Button>Hire me</Button>
       <AppFooter />
+    </section>
+
+    <section class="flex flex-col w-[50%]">
+      <AppHeader />
     </section>
   </section>
 </template>
