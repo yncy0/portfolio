bf4a075933dae5713aabca414ea7218f12e99678
Yncy0 <yncyncy0@gmail.com>
2025-08-01 15:41:24 +0800

    finished: initial layout

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
+import AppFooter from './components/AppFooter.vue'
+import AppHeader from './components/AppHeader.vue'
 import SectionHero from './components/SectionHero.vue'
 import SectionMain from './components/SectionMain.vue'
-
 </script>
 
 <template>
-  <main class="flex flex-col items-start justify-center px-32">
+  <AppHeader />
+  <main class="flex flex-col items-start justify-center px-44">
     <SectionHero />
     <SectionMain />
   </main>
+  <AppFooter />
 </template>
 
 <style scoped></style>
--- src/components/SectionMain.vue
+++ src/components/SectionMain.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import AboutMe from './AboutMe.vue'
-import AppFooter from './AppFooter.vue'
-import AppHeader from './AppHeader.vue'
 import ExperienceList from './ExperienceList.vue'
 import ProjectsList from './ProjectsList.vue'
 
@@ -9,15 +7,13 @@ import { Button } from './ui/button'
 </script>
 
 <template>
-  <section class="flex flex-row min-h-dvh pt-20 gap-28">
-    <section class="flex flex-col w-[50%] gap-5">
+  <section class="flex flex-col min-h-dvh w-full max-w-screen pt-20 gap-28">
+    <section class="flex flex-col gap-5">
       <AboutMe />
       <Button>Hire me</Button>
-      <AppFooter />
     </section>
 
-    <section class="flex flex-col w-[50%] gap-10">
-      <AppHeader />
+    <section class="flex flex-col gap-10">
       <ExperienceList />
       <ProjectsList />
     </section>
