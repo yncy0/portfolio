a7b3727018cdf3b6a296aaf4594082562662077c
Yncy0 <yncyncy0@gmail.com>
2025-08-01 15:00:24 +0800

    finished: prototype

---
+++ src/components/ProjectsCard.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const props = defineProps<{
+  title: string
+  desription: string
+  imageUrl?: string
+  githubUrl?: string
+  projectUrl?: string
+}>()
+</script>
+
+<template>
+  <Card>
+    <CardHeader>
+      <CardTitle>{{ props.title }}</CardTitle>
+      <CardDescription>{{ props.desription }}</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <img :src="props.imageUrl" />
+      <a :href="props.githubUrl">{{ props.githubUrl }}</a>
+    </CardContent>
+    <CardFooter>
+      <Button>
+        <a :href="props.githubUrl">View Project</a>
+      </Button>
+    </CardFooter>
+  </Card>
+</template>
