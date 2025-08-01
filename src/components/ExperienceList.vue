faa146076332aec9cf61bd672a7b8d8607b0eb4e
Yncy0 <yncyncy0@gmail.com>
2025-08-01 14:21:35 +0800

    update: rename Experiences.vue -> ExperienceComponent.vue and Nothing.vue -> NothingCard.vue

--- src/components/ExperienceList.vue
+++ src/components/ExperienceList.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import Nothing from './Nothing.vue';
+import NothingCard from './NothingCard.vue';
 </script>
 
 <template>
   <section id="experience" class="flex flex-col gap-2">
     <h2 class="text-xl">Experiences</h2>
-    <Nothing label="No experience yet!"/>
+    <NothingCard label="No experience yet!"/>
   </section>
 </template>
