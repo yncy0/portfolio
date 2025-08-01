c11e3b45bd4f893e70e52b0463c692a1e57c92cc
Yncy0 <yncyncy0@gmail.com>
2025-08-02 06:25:07 +0800

    finished: toggle button for light and dark theme

---
+++ src/components/ToggleTheme.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { useDark, useToggle } from '@vueuse/core'
+import { Sun, Moon } from 'lucide-vue-next'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <Button
+    class="bg-gruv-bg-500 text-gruv-fg-500 fixed bottom-10 right-20 hover:bg-gruv-bg-500"
+    @click="toggleDark()"
+  >
+    <div v-if="isDark">
+      <Sun />
+    </div>
+    <div v-else>
+      <Moon />
+    </div>
+  </Button>
+</template>
