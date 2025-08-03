7d7f2be8b99539d931c6e642cd71a840e9e26935
Yncy0 <yncyncy0@gmail.com>
2025-08-03 08:44:23 +0800

    added: nuxt project; update: copy some of the folders into nuxt project; update: install dependencies in Nuxt Project to  match the Vue project

---
+++ portfolio/app/components/TechnologyIcon.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { Component } from 'vue'
+import { Card, CardContent } from '@/components/ui/card'
+
+const props = defineProps<{
+  name: string
+  description: string
+  icon: Component
+}>()
+</script>
+
+<template>
+  <Card class="flex flex-col items-center bg-gruv-bg-500 shadow-none border-gruv-fg-500 border-2
+    text-gruv-fg-500">
+    <CardContent class="flex flex-col items-center">
+      <component :is="props.icon" />
+      <h3>{{ props.name }}</h3>
+    </CardContent>
+  </Card>
+</template>
