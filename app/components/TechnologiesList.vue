cb03bbed5bb4702674b80dd90a2288e4c389d0d6
Yncy0 <yncyncy0@gmail.com>
2025-08-06 17:19:37 +0800

    fixed: TechnologyIcon.vue icon size

--- app/components/TechnologiesList.vue
+++ app/components/TechnologiesList.vue
@@ -1,54 +1,44 @@
 <script setup lang="ts">
 type Technologies = {
   name: string
-  description?: string
   icon: string
 }
 
 const technologies: Technologies[] = [
   {
     name: 'Vue',
-    description: 'Simple JavaScript Framework',
     icon: 'simple-icons:vuedotjs',
   },
   {
     name: 'Nuxt',
-    description: 'Vue Meta-Framework',
     icon: 'simple-icons:nuxt',
   },
   {
     name: 'React',
-    description: 'Popular JavaScript Library',
     icon: 'simple-icons:react',
   },
   {
     name: 'Expo',
-    description: 'Modern React Native Framework',
     icon: 'simple-icons:expo',
   },
   {
     name: 'TailwindCSS',
-    description: 'A utility-first CSS framework',
     icon: 'simple-icons:tailwindcss',
   },
   {
     name: 'TypeScript',
-    description: 'JavaScript but with static types',
     icon: 'simple-icons:typescript',
   },
   {
     name: 'Supabase',
-    description: 'Postgres Database',
     icon: 'simple-icons:supabase',
   },
   {
     name: 'Git',
-    description: 'Version control',
     icon: 'simple-icons:git',
   },
   {
     name: 'Neovim',
-    description: 'I use Neovim btw!',
     icon: 'simple-icons:neovim',
   },
 ]
@@ -59,7 +49,7 @@ const technologies: Technologies[] = [
     <h2 class="text-lg lg:text-xl">Technologies</h2>
     <ul class="flex flex-row flex-wrap basis-1/3 gap-1">
       <li v-for="(item, index) in technologies" :key="index" class="flex-1">
-        <TechnologyIcon :name="item.name" :description="item.description" :icon="item.icon"/>
+        <TechnologyIcon :name="item.name" :icon="item.icon"/>
       </li>
     </ul>
   </section>
--- app/components/TechnologyIcon.vue
+++ app/components/TechnologyIcon.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 const props = defineProps<{
   name: string;
-  description: string;
   icon: string;
 }>();
 </script>
@@ -9,7 +8,7 @@ const props = defineProps<{
 <template>
   <Card class="flex flex-col items-center bg-gruv-bg-500 shadow-none border-gruv-fg-500 border-2 text-gruv-fg-500">
     <CardContent class="flex flex-col items-center">
-      <Icon :name="props.icon" />
+      <Icon :name="props.icon" class="size-6"/>
       <h3>{{ props.name }}</h3>
     </CardContent>
   </Card>
