914045072853982ff18f480d03cd64399e13e60b
yncy0 <yncyncy0@gmail.com>
2025-12-29 23:40:18 +0800

    update: used UColorModeButton in ToggleButton.vue and it seems working

--- app/components/toggle/ToggleTheme.vue
+++ app/components/toggle/ToggleTheme.vue
@@ -1,22 +1,5 @@
-<script setup lang="ts">
-import { useDark, useToggle } from "@vueuse/core";
-
-const isDark = useDark();
-const toggleDark = useToggle(isDark);
-
-</script>
-
 <template>
-  <Button
+  <UColorModeButton
     aria-label="Toggle Theme Button"
-    class="bg-base-500 dark:text-ctp-green-200 text-fg-500 dark:border-ctp-green-200 border-fg-500 border hover:bg-base-500"
-    @click="toggleDark()"
-  >
-    <div v-if="isDark">
-      <Icon name="lucide:moon" />
-    </div>
-    <div v-if="!isDark">
-      <Icon name="lucide:sun" />
-    </div>
-  </Button>
+    class="bg-base-500 dark:text-ctp-green-200 text-fg-500 dark:border-ctp-green-200 border-fg-500 border hover:bg-base-500 p-2" />
 </template>
